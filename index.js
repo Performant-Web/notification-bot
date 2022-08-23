8fdf3df1dd5ac63c701de20076d363417e299f46
Mike Melgren <mikemelgren@gmail.com>
2022-08-23 12:45:58 -0500

    chat reading complete

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 require('dotenv').config();
-
 const { Client, GatewayIntentBits } = require('discord.js');
+const { TOKEN } = process.env;
 
-// const { token } = require('./config.json');
-const token = process.env.DISCORD_TOKEN;
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
-// Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
-// When the client is ready, run this code (only once)
-client.once('ready', () => {
-	console.log('Ready!');
+client.once('ready', c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
-// Login to Discord with your client's token
-client.login(token);
+client.on('messageCreate', message => {
+
+    const attachments = message.attachments.map(i => i);
+
+    const imgSize = 80;
+
+    const data = {
+        username: message.author.username,
+        userImg: `https://cdn.discordapp.com/avatars/${message.author.id}/${message.author.avatar}.webp?size=${imgSize}`,
+        message: message.content,
+        messageImg: (attachments.length) ? attachments[0].url : null,
+        server: message.guild.name,
+        serverImg: `https://cdn.discordapp.com/icons/${message.guild.id}/${message.guild.icon}.webp?size=${imgSize}`,
+        channel: message.channel.name,
+        createdAt: message.createdAt,
+        };
+
+    console.log(data);
+
+});
+
+client.login(TOKEN);
