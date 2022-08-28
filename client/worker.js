b7106a8d99d78afd6888d90114bad066f393b7a0
Mike Melgren <mikemelgren@gmail.com>
2022-08-28 00:40:47 -0500

    reworked service worker caching

--- client/worker.js
+++ client/worker.js
@@ -1,21 +1,42 @@
-console.log('Service worker loaded...');
+const cacheName = 'store';
+const cacheFiles = [
+    '/',
+    '/index.html',
+    '/client.js',
+    '/android-chrome-192x192.png',
+    '/android-chrome-512x512.png',
+];
 
 self.addEventListener('install', (e) => {
-    e.waitUntil(
-        caches.open('store').then((cache) => cache.addAll([
-            '/index.html',
-            '/client.js',
-            '/style.css',
-        ])),
-    );
-});
+    console.log('[Service Worker] Install');
+    e.waitUntil((async () => {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(cacheFiles);
+    })());
+  });
 
 self.addEventListener('fetch', (e) => {
-    console.log(e.request.url);
-    e.respondWith(
-        caches.match(e.request).then((response) => response || fetch(e.request)),
-    );
-});
+    e.respondWith((async () => {
+      const r = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (r) { return r; }
+      const response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+  });
+
+  self.addEventListener('activate', (e) => {
+    e.waitUntil(caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key === cacheName) { return; }
+        return caches.delete(key);
+      }));
+    }));
+  });
 
 self.addEventListener('push', (event) => {
     const data = event.data.json();
