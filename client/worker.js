9fc4bb4c1d20f11718b02ba39b7dddbbd88b9522
Mike Melgren <mikemelgren@gmail.com>
2022-08-28 00:21:03 -0500

    added caching service worker

--- client/worker.js
+++ client/worker.js
@@ -1,5 +1,22 @@
 console.log('Service worker loaded...');
 
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open('store').then((cache) => cache.addAll([
+            '/index.html',
+            '/client.js',
+            '/style.css',
+        ])),
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    console.log(e.request.url);
+    e.respondWith(
+        caches.match(e.request).then((response) => response || fetch(e.request)),
+    );
+});
+
 self.addEventListener('push', (event) => {
     const data = event.data.json();
     self.registration.showNotification(
