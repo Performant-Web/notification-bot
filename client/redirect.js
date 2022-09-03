570145f637999ffb95c1608e6c4fe706a0d9ed02
Mike Melgren <mikemelgren@gmail.com>
2022-09-03 10:33:15 -0500

    minimum working version

--- client/redirect.js
+++ client/redirect.js
@@ -1,17 +1,17 @@
 const url = 'https://discord.com/channels/1011637841199112192/1011637841807278084/1015470189120331786'
 // const url = 'discord://discord.com/channels/1011637841199112192/1011637841807278084/1015470189120331786'
-const url = 'discord://discordapp.com/channels/1011637841199112192/1011637841807278084/1015470189120331786'
+// const url = 'discord://discordapp.com/channels/1011637841199112192/1011637841807278084/1015470189120331786'
 // const url = 'https://discordapp.com/channels/1011637841199112192/1011637841807278084/1015470189120331786'
 /*
 let requesting = browser.permissions.request(
     permissions
   )
   */
-
+/*
 setTimeout(() => {
     redirect();
 }, 10000);
-
+*/
 /*
 window.addEventListener('load', (event) =>{
 
@@ -23,7 +23,7 @@ window.addEventListener('load', (event) =>{
 function redirect() {
     window.open(url, '_system');
 }
-
+/*
 const permissionsNames = [
     "geolocation",
     "notifications",
@@ -73,3 +73,4 @@ const permissionsNames = [
   }
 
   console.log(getAllPermissions());
+  */
