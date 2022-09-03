71ec8c6f9038de4daac267d5acf3d6992e8847bf
Mike Melgren <mikemelgren@gmail.com>
2022-09-03 00:13:25 -0500

    window location assign url

--- client/redirect.js
+++ client/redirect.js
@@ -1,2 +1,2 @@
 const w = (window.parent) ? window.parent : window;
-w.location.assign('discord://https://discord.com/channels/1011637841199112192/1011637841807278084/1015470189120331786');
+w.location.assign('discord://discord.com/channels/1011637841199112192/1011637841807278084/1015470189120331786');
