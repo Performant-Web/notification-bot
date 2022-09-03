b7c833de2ada7bfe08cf5a1055d03c8579cc5666
Mike Melgren <mikemelgren@gmail.com>
2022-09-03 00:17:47 -0500

    added alert

--- client/redirect.js
+++ client/redirect.js
@@ -1,2 +1,2 @@
-const w = (window.parent) ? window.parent : window;
-w.location.assign('discord://discord.com/channels/1011637841199112192/1011637841807278084/1015470189120331786');
+alert('JS HERE');
+window.location.assign('discord://discord.com/channels/1011637841199112192/1011637841807278084/1015470189120331786');
