07817faf74ca9b719f598c05dd35d22039b9c9a7
Mike Melgren <mikemelgren@gmail.com>
2022-09-03 00:07:16 -0500

    window location assign protocol

--- client/redirect.js
+++ client/redirect.js
@@ -1,2 +1,2 @@
-console.log('js loaded');
-window.open('https://discord.com/channels/1011637841199112192/1011637841807278084/1015470189120331786');
+const w = (window.parent) ? window.parent : window;
+w.location.assign('discord://https://discord.com/channels/1011637841199112192/1011637841807278084/1015470189120331786');
