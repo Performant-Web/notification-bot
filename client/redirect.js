6622ab5ab9a3877c5d38a581c70f521def9f4e9b
Mike Melgren <mikemelgren@gmail.com>
2022-09-03 10:15:29 -0500

    window.open new url

--- client/redirect.js
+++ client/redirect.js
@@ -1,4 +1,4 @@
-// const url = 'https://discord.com/channels/1011637841199112192/1011637841807278084/1015470189120331786'
+const url = 'https://discord.com/channels/1011637841199112192/1011637841807278084/1015470189120331786'
 // const url = 'discord://discord.com/channels/1011637841199112192/1011637841807278084/1015470189120331786'
 const url = 'discord://discordapp.com/channels/1011637841199112192/1011637841807278084/1015470189120331786'
 // const url = 'https://discordapp.com/channels/1011637841199112192/1011637841807278084/1015470189120331786'
@@ -23,3 +23,53 @@ window.addEventListener('load', (event) =>{
 function redirect() {
     window.open(url, '_system');
 }
+
+const permissionsNames = [
+    "geolocation",
+    "notifications",
+    "push",
+    "midi",
+    "camera",
+    "microphone",
+    "speaker",
+    "device-info",
+    "background-fetch",
+    "background-sync",
+    "bluetooth",
+    "persistent-storage",
+    "ambient-light-sensor",
+    "accelerometer",
+    "gyroscope",
+    "magnetometer",
+    "clipboard",
+    "display-capture",
+    "nfc"
+  ]
+  
+  const getAllPermissions = async () => {
+    const allPermissions = []
+    // We use Promise.all to wait until all the permission queries are resolved
+    await Promise.all(
+      permissionsNames.map(async permissionName => {
+          try {
+            let permission
+            switch (permissionName) {
+              case 'push':
+                // Not necessary but right now Chrome only supports push messages with  notifications
+                permission = await navigator.permissions.query({name: permissionName, userVisibleOnly: true})
+                break
+              default:
+                permission = await navigator.permissions.query({name: permissionName})
+            }
+            console.log(permission)
+            allPermissions.push({permissionName, state: permission.state})
+          }
+          catch(e){
+            allPermissions.push({permissionName, state: 'error', errorMessage: e.toString()})
+          }
+      })
+    )
+    return allPermissions
+  }
+
+  console.log(getAllPermissions());
