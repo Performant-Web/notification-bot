13c16ead8c09c19d230cbb92dfca991699695363
Mike Melgren <mikemelgren@gmail.com>
2022-09-03 01:28:47 -0500

    new url

--- client/redirect.js
+++ client/redirect.js
@@ -1,11 +1,19 @@
-const url = 'https://discord.com/channels/1011637841199112192/1011637841807278084/1015470189120331786'
-// url = 'discord://discord.com/channels/1011637841199112192/1011637841807278084/1015470189120331786'
+// const url = 'https://discord.com/channels/1011637841199112192/1011637841807278084/1015470189120331786'
+const url = 'discord://discord.com/channels/1011637841199112192/1011637841807278084/1015470189120331786'
 // const url = 'discord://discordapp.com/channels/1011637841199112192/1011637841807278084/1015470189120331786'
 // const url = 'https://discordapp.com/channels/1011637841199112192/1011637841807278084/1015470189120331786'
 
 window.onload = (event) =>{
     window.location.assign(url);
 };
+
+/*
+window.addEventListener('load', (event) =>{
+
+    console.log(Page Loaded);
+
+});
+*/
 
 function one() {
     window.location.assign(url);
