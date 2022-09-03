94d0e6fcab3314fbd47adb0f40538a6c15a45e00
Mike Melgren <mikemelgren@gmail.com>
2022-09-03 09:46:42 -0500

    new url

--- client/redirect.js
+++ client/redirect.js
@@ -1,6 +1,6 @@
-const url = 'https://discord.com/channels/1011637841199112192/1011637841807278084/1015470189120331786'
+// const url = 'https://discord.com/channels/1011637841199112192/1011637841807278084/1015470189120331786'
 // const url = 'discord://discord.com/channels/1011637841199112192/1011637841807278084/1015470189120331786'
-// const url = 'discord://discordapp.com/channels/1011637841199112192/1011637841807278084/1015470189120331786'
+const url = 'discord://discordapp.com/channels/1011637841199112192/1011637841807278084/1015470189120331786'
 // const url = 'https://discordapp.com/channels/1011637841199112192/1011637841807278084/1015470189120331786'
 /*
 let requesting = browser.permissions.request(
@@ -8,6 +8,9 @@ let requesting = browser.permissions.request(
   )
   */
 
+setTimeout(() => {
+    redirect();
+}, 10000);
 
 /*
 window.addEventListener('load', (event) =>{
