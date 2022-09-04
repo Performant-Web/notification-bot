c48f4ab1dd3ba169d9efb21552ad555bd62d5391
Mike Melgren <mikemelgren@gmail.com>
2022-09-04 00:54:40 -0500

    redirect first working version

--- client/redirect.js
+++ client/redirect.js
@@ -1,35 +1,9 @@
-const mobile = 'https://discord.com/channels/1011637841199112192/1011637841807278084/1015470189120331786'
-const desktop = 'discord://discord.com/channels/1011637841199112192/1011637841807278084/1015470189120331786'
-/*
-let requesting = browser.permissions.request(
-    permissions
-  )
-  */
+const query = window.location.search;
+const url = query.slice(query.indexOf('=') + 1, query.length);
+const fallback = `https${url.slice(url.indexOf(':'), url.length)}`;
 
-setTimeout(() => {
-    document.getElementById('mycheck').click();
-}, 1000);
-
-/*
-window.addEventListener('load', (event) =>{
-
-    console.log(Page Loaded);
-
+window.addEventListener('load', () => {
+    window.location.replace(url);
 });
-*/
 
-function myFunction() {
-    document.getElementById('mycheck').click();
-}
-
-function mFunction() {
-    document.getElementById('mcheck').click();
-}
-
-function redirect1() {
-    window.open(mobile);
-}
-
-function redirect2() {
-    window.open(desktop);
-}
+document.getElementById('open').href = fallback;
--- client/worker.js
+++ client/worker.js
@@ -11,6 +11,7 @@ const cacheFiles = [
 
 self.addEventListener('install', (e) => {
     console.log('[Service Worker] Install');
+    e.waitUntil(self.skipWaiting());
     e.waitUntil((async () => {
       const cache = await caches.open(cacheName);
       console.log('[Service Worker] Caching all: app shell and content');
@@ -32,6 +33,7 @@ self.addEventListener('fetch', (e) => {
   });
 
   self.addEventListener('activate', (e) => {
+    e.waitUntil(self.clients.claim());
     e.waitUntil(caches.keys().then((keyList) => {
       return Promise.all(keyList.map((key) => {
         if (key === cacheName) { return; }
@@ -51,20 +53,45 @@ self.addEventListener('push', (event) => {
             image: data.image,
             badge: '',
             data: { url: data.url },
-            actions: [{ action: 'open_url', title: 'Read Now' }],
+            // actions: [{ action: 'open_url', title: 'Read Now' }],
         },
     );
 });
 
 self.addEventListener('notificationclick', (event) => {
     // clicking anywhere on notification
-    event.notification.close();
-    clients.openWindow(`/redirect?url=${event.notification.data.url}`);
+    // event.notification.close();
+
+// Attempt to extract notification URL
+const url = `/redirect?url=${event.notification.data.url}`;
+console.log(url);
+
+  // This looks to see if the current is already open and
+  // focuses if it is
+  event.waitUntil(clients.matchAll({
+    type: 'window',
+  }).then((clientList) => {
+    console.log(clientList);
+    for (const client of clientList) {
+      console.log(client);
+      if (client) {
+        console.log('focus');
+        client.navigate(url);
+        client.focus();
+      }
+    }
+    if (clients.openWindow) {
+      console.log('open');
+      clients.openWindow(url);
+    }
+  }));
+
+    // clients.openWindow(`/redirect?url=${event.notification.data.url}`);
     // clicking action button - can add more if needed, maybe dependent on user agent
-    switch (event) {
+    /* switch (event) {
       case 'open_url':
       clients.openWindow(`/redirect?url=${event.notification.data.url}`);
       break;
-    }
+    } */
   }
   , false);
